b4ff4361434f0f50541dd0cce58e648f45d61057
Jean-Marc Flamand <7jtf@videotron.ca>
2022-08-17 10:52:43 -0400

    update source code

--- ExtractEmbededRessources/ExtractEmbededResouces.cs
+++ ExtractEmbededRessources/ExtractEmbededResouces.cs
@@ -8,17 +8,28 @@ using System.IO;
 
 namespace ExtractEmbededRessources
 {
-    internal class ExtractEmbededResouces
+    public class ExtractEmbededResouces
     {
         public static  void ExtractFiles (string nameSpace,
-            string outDirectory,string internalFilePath,string resourceName)
+            string outFile,string internalFilePath,string resourceName)
         {
             Assembly assembly = Assembly.GetCallingAssembly ();
-            using Stream stream = assembly.GetManifestResourceStream(nameSpace + "." + (internalFilePath == "" ? "" : internalFilePath + ".") + resourceName);
-            using BinaryReader reader = new(stream);
-                    using FileStream fileStream = new(resourceName + "\\" + outDirectory, FileMode.OpenOrCreate);
-                        using BinaryWriter writer = new(fileStream);
-            writer.Write(reader.ReadBytes((int)stream.Length));
+            using Stream? stream = assembly.GetManifestResourceStream(nameSpace + "." + (internalFilePath == "" ? "" : internalFilePath + ".") + resourceName);
+            
+            if (stream is not null)
+            {
+                using BinaryReader reader = new(stream);
+                using FileStream fileStream = new(outFile + "\\" + resourceName, FileMode.OpenOrCreate);
+                using BinaryWriter writer = new(fileStream);
+                writer.Write(reader.ReadBytes((int)stream.Length));
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("The rescources file where not found.");
+            }
+            
+                        
         }
 
     }
--- ExtractEmbededRessources/Program.cs
+++ ExtractEmbededRessources/Program.cs
@@ -1,6 +1,31 @@
 ﻿
 // See https://aka.ms/new-console-template for more information
+using System.Reflection;
+
 Console.WriteLine("Press Enter to Extract Sample files.");
 Console.ReadLine();
-ExtractFiles("ExtractEmbededRessources", "c:\\Temp", "EmbededRes", "RichText.rtf");
-ExtractFiles("ExtractEmbededRessources", "c:\\Temp", "EmbededRes", "auguste.PNG");
+ExtractFiles("ExtractEmbededRessources", "c:\\temp2", "EmbededRes", "RichText.rtf");
+ExtractFiles("ExtractEmbededRessources", "c:\\temp2", "EmbededRes", "auguste.PNG");
+
+
+
+//ExtractFiles1("ExtractEmbededRessources", "c:\\temp2", "EmbededRes", "auguste.PNG");
+
+//static void ExtractFiles1(string nameSpace,
+//            string outFile, string internalFilePath, string resourceName)
+//{
+//    Assembly assembly = Assembly.GetCallingAssembly();
+//    using Stream? stream = assembly.GetManifestResourceStream(nameSpace + "." + (internalFilePath == "" ? "" : internalFilePath + ".") + resourceName);
+   
+
+//    if (stream is not null)
+//    {
+//        using BinaryReader reader = new(stream);
+//        using FileStream fileStream = new(outFile + "\\" + resourceName, FileMode.OpenOrCreate);
+//        using BinaryWriter writer = new(fileStream);
+//        writer.Write(reader.ReadBytes((int)stream.Length));
+//        Console.ReadLine();
+//    }
+//    Console.WriteLine("The rescources file where not found.");
+
+//}
