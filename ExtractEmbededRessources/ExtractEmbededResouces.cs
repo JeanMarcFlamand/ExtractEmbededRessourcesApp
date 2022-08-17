3f3e276f4645e89202149266bce8aab4fb031d24
Jean-Flamand <le_beluet@hotmail.com>
2022-08-17 07:24:16 -0400

    Ajoutez des fichiers projet.

---
+++ ExtractEmbededRessources/ExtractEmbededResouces.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
+
+namespace ExtractEmbededRessources
+{
+    internal class ExtractEmbededResouces
+    {
+        public static  void ExtractFiles (string nameSpace,
+            string outDirectory,string internalFilePath,string resourceName)
+        {
+            Assembly assembly = Assembly.GetCallingAssembly ();
+            using Stream stream = assembly.GetManifestResourceStream(nameSpace + "." + (internalFilePath == "" ? "" : internalFilePath + ".") + resourceName);
+            using BinaryReader reader = new(stream);
+                    using FileStream fileStream = new(resourceName + "\\" + outDirectory, FileMode.OpenOrCreate);
+                        using BinaryWriter writer = new(fileStream);
+            writer.Write(reader.ReadBytes((int)stream.Length));
+        }
+
+    }
+}
---
+++ ExtractEmbededRessources/Program.cs
@@ -0,0 +1,6 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Press Enter to Extract Sample files.");
+Console.ReadLine();
+ExtractFiles("ExtractEmbededRessources", "c:\\Temp", "EmbededRes", "RichText.rtf");
+ExtractFiles("ExtractEmbededRessources", "c:\\Temp", "EmbededRes", "auguste.PNG");
